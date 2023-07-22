598836941c92564bda45efefcf14da11c54ee1a3
hardimansa <48335935+hardimansa@users.noreply.github.com>
2023-07-22 12:10:30 -0500

    Update admin_read.py

--- admin_read.py
+++ admin_read.py
@@ -1,42 +1,29 @@
-import os
+def Auditor(configfile, auditfile):
+        configfile = configfile
+        auditfile = auditfile
 
-directory = 'configs'
-output_file = 'combined.txt'
+
+        # open configuration, with read priv / format outputs 
+        with open(configfile, 'r') as config:
+                    x = ("{}".format(config.read()))
+                    conf_final = (x.lstrip().splitlines())
+
+        # open audit file, with read priv / format outputs 
+        with open(auditfile, 'r') as audit:
+                    y = ("{}".format(audit.read()))
+                    audit_final = (y.lstrip().splitlines())
+
+        ### check for instances of your keyword(s) against lines in the configuraiton file
+        for lines in conf_final:
+                for keywords in audit_final:
+                        if keywords in lines:
+                                print(lines)
 
 
-### Combine config files in the directory ###
-with open(output_file, "a") as outfile:
-   for filename in os.listdir(directory):
-      filepath = os.path.join(directory, filename)
-      if os.path.isfile(filepath):
-         with open(filepath, 'r') as infile:
-            contents = infile.read()
-            outfile.write(contents + '\n')
+if __name__ == '__main__':
+    Auditor('exampleconfig.txt', 'exampleaudit.txt')
 
-### Populate "keyword.txt with information used in manual STIG
 
-z="Audit Keyword Matched"
-
-#configs = [" 192.168.56.10", " 192.168.56.11", " 192.168.56.12", " 192.168.56.13", " 192.168.56.14"]
-configs = ["combined.txt"]
-
- # STIG reference checklist (keyword)
-with open('keyword.txt', 'r') as f:
-    keyword = f.read().splitlines()
-
-# configuration files to be audited
-for v in configs:
-    print(f'Reading {v}') 
-    with open(v, 'r') as f: 
-#with open('1.ios', 'r') as f:
-     lines = f.readlines()
-     
-
-# Search for each keyword in the file
-for keyword in keyword:
-    for line in lines:
-        if keyword in line:
-           print(f' \n \v \t "{z}" "{line.rstrip()}" ')
 
 
 
